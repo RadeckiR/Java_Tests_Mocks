b1cf3ce92fac7f87d857565cb8035d0a46755bc9
RadeckiR <radecki.radoslaw@gmail.com>
2020-11-04 17:26:19 +0100

    example-file

---
+++ 04-git/Program.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.math.BigDecimal;
+import java.time.LocalTime;
+
+public class Drink extends Product{
+     Drink(int id, String name, BigDecimal price, Rating rating) {
+        super(id, name, price, rating);
+    }
+
+    @Override
+    public Product applyRating(Rating newRating) {
+        return new Drink(getId(), getName(),getPrice(),newRating);
+    }
+
+    @Override
+    public BigDecimal getDiscount() {
+        if ( LocalTime.now().isBefore(LocalTime.of(17,30)) && LocalTime.now().isAfter(LocalTime.of(17,30))){
+            return super.getDiscount();
+        }
+        else{
+            return BigDecimal.ZERO;
+        }
+
+    }
+}
